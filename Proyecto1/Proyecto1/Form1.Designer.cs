033730c7380de62e10a2343d5907fc9957b70440
nicomas02 <nicomasforner@gmail.com>
2022-05-08 22:31:38 +0200

    Cuarto push

--- Proyecto1/Proyecto1/Form1.Designer.cs
+++ Proyecto1/Proyecto1/Form1.Designer.cs
@@ -82,9 +82,9 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(572, 33);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.Size = new System.Drawing.Size(32, 13);
             this.label3.TabIndex = 5;
-            this.label3.Text = "Telefono";
+            this.label3.Text = "Movil";
             // 
             // textBox1
             // 
